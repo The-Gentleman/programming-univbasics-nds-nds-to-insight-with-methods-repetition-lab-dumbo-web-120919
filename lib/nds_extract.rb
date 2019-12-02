5811cabc876fe9d7abb48dd873106ee8d5493362
Zeshan Raja <zraja4402@gmail.com>
2019-12-02 17:10:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -72,7 +72,7 @@ director_counter = 0
 result = {
 } 
  
-while director_counter < source.length do 
+
  director_name = source[director_counter][:name] 
  movie_counter = 0
  result[director_name] = 0 
@@ -80,12 +80,9 @@ while director_counter < source.length do
   while movie_counter < source[director_counter][:movies].count do 
    result[director_name] += source[director_counter][:movies][movie_counter][:worldwide_gross]
    movie_counter += 1 
+   director_counter += 1 
   end 
 
-  director_counter += 1 
-
-
-end 
 result 
 end
 
